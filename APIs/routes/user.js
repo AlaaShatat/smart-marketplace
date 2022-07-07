011b17cb5350f4913835b123ada8db155380d3ea
Alaa Shatat <alaashabaan102210@gmail.com>
2022-07-07 10:10:03 +0200

    Add recommendation for the user

--- APIs/routes/user.js
+++ APIs/routes/user.js
@@ -3,7 +3,7 @@ const { update } = require('lodash');
 const router  = express.Router();
 // controllers
 const { requireSignin, isAuth, isAdmin, isSeller } = require('../controllers/auth');
-const { userById , read, updateUser, purchaseHistory} = require('../controllers/user');
+const { userById , read, updateUser, purchaseHistory,recommendList, recommendProducts} = require('../controllers/user');
 
 //routers
 router.get("/secret/:userId", requireSignin,isAuth, isAdmin, (req, res)=>{
@@ -16,6 +16,9 @@ router.get("/secret/:userId", requireSignin,isAuth, isAdmin, (req, res)=>{
 router.get("/user/:userId", requireSignin, isAuth, read);
 router.put("/user/:userId", requireSignin, isAuth, updateUser);
 router.get("/orders/by/user/:userId", requireSignin, isAuth, purchaseHistory);
+router.get("/user/recommend/:userId", requireSignin, isAuth, recommendList);
+//router.get("/user/recommend/products/:userId", requireSignin, isAuth, recommendProducts);
+
 
 router.param('userId', userById);
 
