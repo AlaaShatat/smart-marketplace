212b973a1064ba2d70726456c3bd94ef1fb4f9a9
Alaa Shatat <alaashabaan102210@gmail.com>
2022-07-08 07:08:46 +0200

    update checkout
    - free shipping for loyal customers

--- Frontend/src/core/apiCore.js
+++ Frontend/src/core/apiCore.js
@@ -1,15 +1,16 @@
 import { API } from "../config";
 import queryString from "query-string";
 
-export const getProducts = sortBy => {
+export const getProducts = async sortBy => {
     //console.log("inside getproducts")
-    return fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=6`, {
-        method: "GET"
-    })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => console.log(err));
+    try {
+        const response = await fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=6`, {
+            method: "GET"
+        });
+        return await response.json();
+    } catch (err) {
+        return console.log(err);
+    }
 };
 
 export const getCategories = () => {
