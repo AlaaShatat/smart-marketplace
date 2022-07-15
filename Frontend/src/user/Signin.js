0776bf2223abd67f3fe3981ce916eaaf152476bd
Alaa Shatat <alaashabaan102210@gmail.com>
2022-07-16 01:49:05 +0200

    update title

--- Frontend/src/user/Signin.js
+++ Frontend/src/user/Signin.js
@@ -95,7 +95,7 @@ const Signin = () => {
     return (
         <Layout
             title="Signin"
-            description="Signin to Node React E-commerce App"
+            description="Signin and Let The Adventure Begin!"
             className="container col-md-8 offset-md-2"
         >
             {showLoading()}
